cf7d492a086398594e752fa8febdb6e9f20381d4
brianroyee <briancodee@gmail.com>
2025-03-31 15:54:10 +0530

    Daily commit: 2025-03-31

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ with open(os.path.join(repo_directory, "hello.html"), "w") as f:
         f.write(f"<html><body><h1>Hello World!</h1><p>{datetime.now().strftime('%Y-%m-%d')}</p></body></html>")
 
 os.chdir(repo_directory)
-os.system("git add .")
+os.system("git add hello.html")
 os.system(f'git commit -m "Daily commit: {datetime.now().strftime("%Y-%m-%d")}"')
 os.system(f"git push {git_remote} {git_branch}")
 
