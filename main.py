abf947cdf1e845c7bdddd0bde78e8b4f46f853f1
brianroyee <briancodee@gmail.com>
2025-03-31 15:42:05 +0530

    Daily commit: 2025-03-31

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from datetime import datetime
+
+repo_directory = "/home/teknikal/Desktop/orbital simulator/autopush/"
+git_remote = "origin"
+git_branch = "main"
+
+with open(os.path.join(repo_directory, "hello.html"), "w") as f:
+        f.write(f"<html><body><h1>Hello World!</h1><p>{datetime.now().strftime('%Y-%m-%d')}</p></body></html>")
+
+os.chdir(repo_directory)
+os.system("git add .")
+os.system(f'git commit -m "Daily commit: {datetime.now().strftime("%Y-%m-%d")}"')
+os.system(f"git push {GIT_REMOTE} {GIT_BRANCH}")
+
